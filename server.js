ffa629aaf92bd846c2b0c96dd28456f708c44e31
Faez Bhatti <faezbhati@gmail.com>
2022-04-11 19:44:36 +0200

    Update server.js for database

--- server.js
+++ server.js
@@ -5,3 +5,14 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('public/db/db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const dbPort = process.env.PORT || 3004; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
