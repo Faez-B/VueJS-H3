ab37a204652132a3534ac63bcab9daa032642a87
Faez Bhatti <faezbhati@gmail.com>
2022-04-11 19:32:44 +0200

    Add server.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "VueJS-H3",
+  // base : "VueJS-H3",
   plugins: [vue()],
   resolve: {
     alias: {
